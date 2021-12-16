e90099150f0b3f265c93c285ced6ceb498cdf9ca
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2021-12-16 17:25:04 +0200

    push1

--- app/handlers.py
+++ app/handlers.py
@@ -1,9 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body, Depends
+from app.forms import UserLoginForm
 
 
 router = APIRouter()
 
-
-@router.get('/')
+@router.post('/')
 def index():
+  return {'status': "OK"}
+
+
+@router.post('/login', name='user:login')
+def login(user_form: UserLoginForm = Body(..., embed=True), database= ):
+  
   return {'status': "OK"}
